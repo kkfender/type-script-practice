e9a7bd4bbce5e2a1dfb05d03e074cdfe3e5d1a5b
kkfender <ibanez19851030@yahoo.co.jp>
2025-03-22 17:34:59 +0900

    プリミティブ型の型推論

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const message:string = "Hello, World!";
-console.log(message);
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter your name: ", (line) => {
+  console.log(line + 1000);
+  rl.close();
+});
